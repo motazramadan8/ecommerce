745e0e9159e5338b092d0a5d92fd6ac5c04f4db5
Motaz Ramadan <120125123+motazramadan8@users.noreply.github.com>
2023-01-26 16:44:41 +0200

    Add files via upload

---
+++ src/Price/Price.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import NavBar from '../NavBar'
+import Header from './Header'
+import Pricee from '../Home/Pricee'
+import Footer from '../Footer'
+
+const Price = () => {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+      <br />
+      <br />
+      <br />
+      <br />   
+      <br />
+      <Pricee />
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </div>
+  )
+}
+
+export default Price
